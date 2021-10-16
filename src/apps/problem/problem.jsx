57e507ca2033e1f8ed6e95636d3cb84248dadd9e
Mahmudul Alam <mahmudula2000@gmail.com>
2021-10-16 20:50:05 +0600

    Problem notice now render dangerously

--- src/apps/problem/problem.jsx
+++ src/apps/problem/problem.jsx
@@ -8,57 +8,58 @@ import { Table } from "../../components/customTags";
 import ProblemCodeForm from "../../components/forms/problemCodeForm";
 
 const Problem = ({ match, history }) => {
-   const { problemActs, userActs } = useContext(SuperContext);
-   const { problemId } = match.params;
-   const problem = problemActs.getById(problemId);
+  const { problemActs, userActs } = useContext(SuperContext);
+  const { problemId } = match.params;
+  const problem = problemActs.getById(problemId);
 
-   const userId = getCurrentUser() && getCurrentUser().user_id;
-   return (
-      problem && (
-         <div className="container">
-            {userId && problem.user === userId && (
-               <div>
-                  <Link to={`${urls.editProblem}/${problemId}`} className={"btn btn-warning"}>
-                     Edit
-                  </Link>
-               </div>
-            )}
+  const userId = getCurrentUser() && getCurrentUser().user_id;
+  return (
+    problem && (
+      <div className="container">
+        {userId && problem.user === userId && (
+          <div>
+            <Link to={`${urls.editProblem}/${problemId}`} className={"btn btn-warning"}>
+              Edit
+            </Link>
+          </div>
+        )}
 
-            <div className="float-end">
-               <a className={"white-link"} href={"#codeEditor"}>
-                  Code Editor
-               </a>
-            </div>
-            <div className={"text-center pt-5"}>
-               <h1 className={"display-4 fw-bold text-secondary rounded-3"}>{problem.title}</h1>
-               <p> Writer: {userActs.fullName(problem.user)} </p>
-               <p>Time Limit: {problem.memory_limit} MB</p>
-               <p>Memory Limit: {problem.time_limit} second(s)</p>
-            </div>
+        <div className="float-end">
+          <a className={"white-link"} href={"#codeEditor"}>
+            Code Editor
+          </a>
+        </div>
+        <div className={"text-center pt-5"}>
+          <h1 className={"display-4 fw-bold text-secondary rounded-3"}>{problem.title}</h1>
+          <p> Writer: {userActs.fullName(problem.user)} </p>
+          <p>Time Limit: {problem.memory_limit} MB</p>
+          <p>Memory Limit: {problem.time_limit} second(s)</p>
+        </div>
 
-            <div>
-               {problem.notice && <div className={"alert alert-secondary"}>{problem.notice}</div>}
-               <b className={"h5"}>Problem statement</b>
-               <FormattedHtml text={problem.description} /> <br />
-               <b className={"h5"}>Input Terms</b>
-               <FormattedHtml text={problem.input_terms} /> <br />
-               <b className={"h5"}>Output Terms</b>
-               <FormattedHtml text={problem.output_terms} /> <br />
-            </div>
-           <b className={"h5"}>Examples</b>
-            <TestCases test_cases={problem.test_cases} />
-            <ProblemCodeForm problem={problem} history={history} />
-         </div>
-      )
-   );
+        <div>
+          {problem.notice && <div className={"alert alert-secondary"}>
+            <FormattedHtml text={problem.notice} /></div>}
+          <b className={"h5"}>Problem statement</b>
+          <FormattedHtml text={problem.description} /> <br />
+          <b className={"h5"}>Input Terms</b>
+          <FormattedHtml text={problem.input_terms} /> <br />
+          <b className={"h5"}>Output Terms</b>
+          <FormattedHtml text={problem.output_terms} /> <br />
+        </div>
+        <b className={"h5"}>Examples</b>
+        <TestCases test_cases={problem.test_cases} />
+        <ProblemCodeForm problem={problem} history={history} />
+      </div>
+    )
+  );
 };
 
 export default Problem;
 
 const TestCases = ({ test_cases }) => {
-   const data = [];
-   for (let test of test_cases) {
-      data.push([<pre>{test.inputs}</pre>, <pre>{test.output}</pre>]);
-   }
-   return <Table headers={["Input", "Output"]} body={data} />;
+  const data = [];
+  for (let test of test_cases) {
+    data.push([<pre>{test.inputs}</pre>, <pre>{test.output}</pre>]);
+  }
+  return <Table headers={["Input", "Output"]} body={data} />;
 };
