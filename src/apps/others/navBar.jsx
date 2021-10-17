86d300379a99fde923b205c0007aef592b21aa1f
Mahmudul Alam <mahmudula2000@gmail.com>
2021-10-17 10:42:07 +0600

    added register field

--- src/apps/others/navBar.jsx
+++ src/apps/others/navBar.jsx
@@ -6,73 +6,81 @@ import { urls } from "../../configuration";
 import { logout } from "../../components/authService";
 
 const NavBar = () => {
-   const { userActs } = useContext(SuperContext);
-   const user = userActs.currentUser();
-   return (
-      <nav className={css.navbar} id={"navBar1"}>
-         <div className={"container-fluid"}>
-            <button
-               className="navbar-toggler"
-               type="button"
-               data-bs-toggle="collapse"
-               data-bs-target="#navbarSupportedContent"
-               aria-controls="navbarSupportedContent"
-               aria-expanded="false"
-               aria-label="Toggle navigation"
-            >
-               <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-               <ul className="navbar-nav">
-                  <li className="nav-item">
-                     <NavLink className="nav-link" to={urls.home}>
-                        Home
-                     </NavLink>
-                  </li>
-                  <li className="nav-item">
-                     <NavLink className="nav-link" to={urls.contests}>
-                        Contests
-                     </NavLink>
-                  </li>
-                  <li className="nav-item">
-                     <NavLink className="nav-link" to={urls.problems}>
-                        Problems
-                     </NavLink>
-                  </li>
-                  <li className="nav-item">
-                     <NavLink className="nav-link" to={urls.submissions}>
-                        Submission
-                     </NavLink>
-                  </li>
-                  <li className="nav-item">
-                     <NavLink className="nav-link" to={urls.tutorials}>
-                        Tutorials
-                     </NavLink>
-                  </li>
-                  <li className="nav-item">
-                     {!user && (
-                        <NavLink to={urls.login} className="nav-link">
-                           Login
-                        </NavLink>
-                     )}
-                     {user && (
-                        <NavLink to={urls.profile} className="nav-link">
-                           Profile
-                        </NavLink>
-                     )}
-                  </li>
-                  {user && (
-                     <li className={"nav-item"}>
-                        <button onClick={logout} className={"btn btn-sm fw-bold btn-outline-danger nav-link"}>
-                           Logout
-                        </button>
-                     </li>
-                  )}
-               </ul>
-            </div>
-         </div>
-      </nav>
-   );
+  const { userActs } = useContext(SuperContext);
+  const user = userActs.currentUser();
+  return (
+    <nav className={css.navbar} id={"navBar1"}>
+      <div className={"container-fluid"}>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to={urls.home}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={urls.contests}>
+                Contests
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={urls.problems}>
+                Problems
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={urls.submissions}>
+                Submission
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={urls.tutorials}>
+                Tutorials
+              </NavLink>
+            </li>
+
+            {!user &&
+            <li className="nav-item">
+              <NavLink to={urls.register} className="nav-link">
+                Register
+              </NavLink>
+            </li>}
+            {!user &&
+            <li className="nav-item">
+              <NavLink to={urls.login} className="nav-link">
+                Login
+              </NavLink>
+            </li>}
+            {user && <li className="nav-item">
+              <NavLink to={urls.profile} className="nav-link">
+                Profile
+              </NavLink>
+            </li>
+            }
+
+            {user && (
+              <li className={"nav-item"}>
+                <button onClick={logout} className={"btn btn-sm fw-bold btn-outline-danger nav-link"}>
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
